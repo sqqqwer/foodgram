fe303b9b24c9281e29dc0115923dacd183d8e2d3
Владислав Чернявский <cernavskijv877@gmail.com>
2025-01-21 17:49:09 +1000

    Add new paginations class.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -134,8 +134,7 @@ REST_FRAMEWORK = {
         'anon': '1000/day',
     },
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 6,
+    'DEFAULT_PAGINATION_CLASS': 'api.paginations.LimitPageNumberPagination',
 }
 
 DJOSER = {
